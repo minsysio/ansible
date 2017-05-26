118ec55b108a68034a14b3398b9d0b2a543d23d6
minsyslvi <lvi@minsys.io>
2017-05-25 22:10:48 -0400

    Python interpreter for Xenial

--- _launchpad/launchpad.sh
+++ _launchpad/launchpad.sh
@@ -12,6 +12,6 @@ chmod u+x $LAUNCHPAD_DIR/launchpad.sh
 :> /root/.ssh/known_hosts
 read -p 'Enter the new host/minion ip adress  : ' ip_minion
 #Need Control structure for OVH / Other provider -> changing "ansible_ssh_user= && base_minion_cloud=$Provider
-echo "[base_minion_cloud]\n$ip_minion\n[base_minion_cloud:vars]\nhost_key_checking= False\nansible_ssh_user= ubuntu123\nansible_ssh_private_key_file= /root/.ssh/ansible_rsa " > /etc/ansible/inventories/cloud.minsys.io/hosts
+echo "[base_minion_cloud]\n$ip_minion\n[base_minion_cloud:vars]\nhost_key_checking= False\nansible_ssh_user= ubuntu123\nansible_ssh_private_key_file= /root/.ssh/ansible_rsa\nansible_python_interpreter=/usr/bin/python3" > /etc/ansible/inventories/cloud.minsys.io/hosts
 ansible-playbook /etc/ansible/playbooks/base_minion_cloud/deploy_ovh.yml 
 exit
