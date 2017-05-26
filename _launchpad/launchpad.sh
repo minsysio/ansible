ba2fd353e66454ca02d899cd9048a5f420837c49
minsyslvi <lvi@minsys.io>
2017-05-25 21:41:54 -0400

    Before first launch

--- _launchpad/launchpad.sh
+++ _launchpad/launchpad.sh
@@ -13,5 +13,5 @@ chmod u+x $LAUNCHPAD_DIR/launchpad.sh
 read -p 'Enter the new host/minion ip adress  : ' ip_minion
 #Need Control structure for OVH / Other provider -> changing "ansible_ssh_user= && base_minion_cloud=$Provider
 echo "[base_minion_cloud]\n$ip_minion\n[base_minion_cloud:vars]\nhost_key_checking= False\nansible_ssh_user= ubuntu\nansible_ssh_private_key_file= /root/.ssh/ansible_rsa " > /etc/ansible/inventories/cloud.minsys.io/hosts
-#ansible-playbook /etc/ansible/playbooks/base_system_cloud/deploy_ovh
+ansible-playbook /etc/ansible/playbooks/base_minion_cloud/deploy_ovh.yml
 exit
