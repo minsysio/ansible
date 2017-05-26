3c758ae0aafffa759276d44788f8d419cbad46f6
minsyslvi <lvi@minsys.io>
2017-05-25 21:27:18 -0400

    Modify "echo" syntax

--- _launchpad/launchpad.sh
+++ _launchpad/launchpad.sh
@@ -12,6 +12,6 @@ chmod u+x $LAUNCHPAD_DIR/launchpad.sh
 :> /root/.ssh/known_hosts
 read -p 'Enter the new host/minion ip adress  : ' ip_minion
 #Need Control structure for OVH / Other provider -> changing "ansible_ssh_user= && base_minion_cloud=$Provider
-echo -e "[base_minion_cloud]\n$ip_minion\n[base_minion_cloud:vars]\nhost_key_checking= False\nansible_ssh_user= ubuntu\nansible_ssh_private_key_file= /root/.ssh/ansible_rsa " > /etc/ansible/inventories/cloud.minsys.io/hosts
+echo "[base_minion_cloud]\n$ip_minion\n[base_minion_cloud:vars]\nhost_key_checking= False\nansible_ssh_user= ubuntu\nansible_ssh_private_key_file= /root/.ssh/ansible_rsa " > /etc/ansible/inventories/cloud.minsys.io/hosts
 #ansible-playbook /etc/ansible/playbooks/base_system_cloud/deploy_ovh
 exit
