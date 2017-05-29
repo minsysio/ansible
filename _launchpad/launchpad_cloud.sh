e49599acb8bb24a8762200beed6ff585afe9af60
minsyslvi <lvi@minsys.io>
2017-05-29 11:10:54 -0400

    Improved launchpad script

--- _launchpad/launchpad_cloud.sh
+++ _launchpad/launchpad_cloud.sh
@@ -10,14 +10,17 @@ ANSIBLE_CONFGIT_DIR=/opt/ansible
 ANSIBLE_CONFGIT_TRUNK_DIR=$ANSIBLE_CONFGIT_DIR/trunk
 LAUNCHPAD_DIR=$ANSIBLE_CONFGIT_TRUNK_DIR/_launchpad 
 
-
+echo "Local Repository upate"
 cd $ANSIBLE_CONFGIT_DIR
 svn update
+echo "Copy Repository = last updated working configuration, to Ansible Configuration Directory -> $ANSIBLE_CONF_DIR"
 cp -R $ANSIBLE_CONFGIT_TRUNK_DIR/* $ANSIBLE_CONF_DIR
 chmod u+x $LAUNCHPAD_DIR/launchpad_cloud.sh
 :> /root/.ssh/known_hosts
 read -p 'Enter the ++MINION++ IP address  : ' ip_minion
 #Need Control structure for OVH / Other provider -> changing "ansible_ssh_user= && base_minion_cloud=$Provider
+echo "Ansible Inventory Initilization"
 echo "[base_minion_cloud]\n$ip_minion\n[base_minion_cloud:vars]\nansible_user= ubuntu\nansible_ssh_private_key_file= /root/.ssh/ansible_rsa\nansible_python_interpreter=/usr/bin/python3" > $ANSIBLE_INVENTORY_DIR/hosts
+echo "Launch Ansible PLAYBOOK for minion : $ip_minion"
 ansible-playbook $ANSIBLE_PLAYBOOKS_DIR/deploy_ovh.yml 
 exit
