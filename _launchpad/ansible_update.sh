f3a4afd6f4c13597e0b89c0a0384c21b3b47cb6f
lvi <lvi@minsys.io>
2018-02-15 15:03:23 -0500

    Added Ansible update

---
+++ _launchpad/ansible_update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+#To update Ansible Directory after massive change - only SVN UPDATE + CP to ANSIBLE diretory 
+
+#Q : lvi@minsys.io
+
+ANSIBLE_CONF_DIR=/etc/ansible
+ANSIBLE_CONFGIT_DIR=opt/ansible
+ANSIBLE_CONFGIT_TRUNK_DIR=$ANSIBLE_CONFGIT_DIR/trunk
+
+echo "Local Repository upate"
+cd $ANSIBLE_CONFGIT_DIR
+svn update
+
+echo "Copy Repository & last updated working configuration to Ansible Configuration Directory"
+cp -r $ANSIBLE_CONFGIT_TRUNK_DIR/* $ANSIBLE_CONF_DIR
